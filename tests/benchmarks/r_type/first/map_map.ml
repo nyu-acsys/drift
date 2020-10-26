3df01effc3817b82aeb0397670c44f1aea16c8b1
wies <wies@cs.nyu.edu>
2020-10-25 20:34:23 -0400

    eliminate minor syntactic differences to original benchmarks

--- tests/benchmarks/r_type/first/map_map.ml
+++ tests/benchmarks/r_type/first/map_map.ml
@@ -1,7 +1,5 @@
-
-
 let rec map x =
- if x = 0 then x else 1 + map (x - 1)
+ if x=0 then x else 1 + map (x-1)
 
 let main (n:int(*-:{v:Int | true}*)) =
-    assert(map (map n) = n)
+  assert(map (map n) = n)
