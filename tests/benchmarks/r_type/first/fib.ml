ffe1f0f6a138692633a6162759e4898a6a19331a
wies <wies@cs.nyu.edu>
2020-10-26 21:21:44 -0400

    eliminate minor syntactic differences to original benchmarks

--- tests/benchmarks/r_type/first/fib.ml
+++ tests/benchmarks/r_type/first/fib.ml
@@ -1,7 +1,6 @@
-
 let rec fib n =
-  if n < 2 then 1 else
-    fib (n - 1) + fib (n - 2)
+  if n<2 then 1 else
+    fib (n-1) + fib (n-2)
 
-let main (n:int(*-:{v:Int | true}*)) =
-    assert (fib n >= n)
+let main (n (*-:{v:Int | true}*)) =
+  assert (n <= fib n)
