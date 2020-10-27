4bb54c5b4625f6452627aeefa2073d521d36c135
wies <wies@cs.nyu.edu>
2020-10-27 04:25:26 -0400

    eliminate minor syntactic differences to original benchmarks

--- tests/benchmarks/r_type/high/exc-fact.ml
+++ tests/benchmarks/r_type/high/exc-fact.ml
@@ -3,15 +3,11 @@ USED: PLDI2011 as e-fact
 USED: PEPM2013 as e-fact
 *)
 
-
 let rec fact n exn =
   if n <= 0 then
     exn 0
   else
-    let exnp n = if n = 0 then 1 else exn n in
-    n * fact (n - 1) exnp
-
-let exni n = assert false
-
-let main (n:int(*-:{v:Int | true}*)) =
-    if n > 0 then (fact n exni; ())
+    let exn n = if n = 0 then 1 else exn n in
+    n * fact (n - 1) exn
+let exn n = assert false
+let main (n (*-:{v:Int | true}*)) = if n > 0 then (fact n exn; ())
