84e899d3ecb9319e9642ec37b8ed5984a3572788
disconsis <ketan.kanishka@nyu.edu>
2021-10-22 22:55:39 +0530

    fix resources example

--- tests/effects/resources.ml
+++ tests/effects/resources.ml
@@ -1,20 +1,33 @@
 let ev d _s = _s + d
 
-(* infer upper bound on return value  of 400+40400*length(data) *)
-let rec help data:uint[] i:uint totalSupply:uint _s0 : int =
-  if i<data.length then 
-      let _s1 = ev 1 _s0 in (***) let a = address(  data[i] & D160-1 ) in
-      let _s2 = ev 1 _s1 in (***) let amount = data[i] / D160 in
-      if (balanceOf[a] == 0) then begin
-        let _s3 = ev 1 _s2 in (***) balanceOf[a] = amount;
-        let _s4 = ev 1 _s3 in (***) help data (i+1) (totalSupply + amount) _s4; 
-      end else 
-        let _s3 = ev 1 _s2 in (***)  help data (i+1) totalSupply _s3
-  else
-    _s0 (***)  
+let nondet (i:int) = (i * i * i * 500) mod 199
 
-let fill(uint[] data) =
-  let _s1 = 0 in
-  if ((msg.sender != owner)||(Sealed)) 
-    begin ev 1 _s; throw; end
-  else begin ev 1 _s; help data 0 _s1 end 
+let rec help (i:int) (length:int) (_s0:int) =
+  if i < length then
+    let _s1 = ev 1 _s0 in
+    let _s2 = ev 1 _s1 in
+    if nondet i > 100 then
+      let _s3 = ev 1 _s2 in
+      let _s4 = ev 1 _s3 in
+      help (i+1) length _s4
+    else
+      let _s5 = ev 1 _s2 in
+      help (i+1) length _s5
+  else
+    _s0
+
+let fill (length:int) =
+  let _s00 = 0 in
+  let _s01 = ev 1 _s00 in
+  help 0 length _s01
+  (** breaking version **)
+  (* let _s0 = 0 in
+  let _s1 = ev 1 _s0 in
+  help 0 length _s1 *)
+
+let main (size:int (*-:{v:Int | true}*)) =
+  if size > 0 then
+    let final = fill size in
+    assert false
+  else
+    ()
