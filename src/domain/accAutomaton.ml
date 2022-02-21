82ee8c1b80b997f2b37bf7558ca4a83c19415b08
Eric Koskinen <eric.koskinen@stevens.edu>
2022-02-21 22:08:13 +0000

    sketch of an accumulating automaton

---
+++ src/domain/accAutomaton.ml
@@ -0,0 +1,35 @@
+(* An Accumulating Automaton *)
+
+module type AccAut =
+  sig
+    type acc_t
+    type qs_t
+
+    val to_term_init  : unit -> DriftSyntax.term
+    val to_term_ev    : unit -> DriftSyntax.term
+    val to_term_final : unit -> DriftSyntax.term
+    val to_string : unit -> string
+end
+
+type last_qs = LastQ0
+
+module LastAA = AccAut(struct 
+  type acc_t = int (* save the last one *)
+  type qs_t = last_qs
+  let to_term_init () = 
+    let l = {pl=666;pc=666} in 
+       (* AccAut set the initial automaton state and accumulator value *)
+       TupleLst [LastQ0;Const(Boolean(false),l);Const(Integer(1),l)]
+  let to_term_ev () = 
+    mk_lambda (Var("d",l)) (
+       mk_lambda (Var("acc",l)) (
+           (* AccAut Transition function here: *)
+           TupleLst [LastQ0;Const(Boolean(true),l);Var("d",l)] (* todo - make "d" from acc_t *)
+       )
+    )
+  let to_term_final () = 
+    mk_lambda (Var("acc",l)) (
+        (* AccAut check if Accepting state or property of accumulator: *)
+        App( (* assert *), BinOp(Gt,(Var("acc",l)),Const(Integer(5)),l))
+    )
+end)
