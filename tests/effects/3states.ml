ee3122443214f272f4ae18a17d2856e583ff1247
Eric Koskinen <eric.koskinen@stevens.edu>
2022-03-14 15:37:48 +0000

    3states example

--- tests/effects/3states.ml
+++ tests/effects/3states.ml
@@ -8,10 +8,10 @@ let ev d _s =
 let nondet (i:int) = (i * i * i * 500) mod 199
 
 (* at least 3 events happen, then only "v" events *)
-let rec aut n v _s0 =
+let rec aut n _s0 =
    if n > 0 then begin
       let _s1 = ev n _s0 in
-      aut v _s1 
+      aut (n-1) _s1 
   end else _s0
 
 let main (n:int(*-:{v:Int | true}*)) = 
