a8dba67a617eec6bbb8d3d4c09a9507e1bebda6d
Eric Koskinen <eric.koskinen@stevens.edu>
2022-03-21 13:18:46 +0000

    sketches of user input

--- tests/effects/3states-prop.ml
+++ tests/effects/3states-prop.ml
@@ -1,11 +1,14 @@
 let init = 
   (0,0)
 
-let trans q acc = 
-  match (q,acc) with 
+let trans q_acc = 
+  match q_acc with 
   | (0,_) -> 1
   | (1,_) -> 2
   | (2,_) -> 3
   | (3,_) -> 3
 
-let final q acc = (q = 3)
+let final q_acc = match q_acc with | (q,acc) -> (q = 3)
+ 
+let main =
+  if final (trans init) then assert(true)
