4244ed5796482ac6f34a996a3f549a2ab9756293
Mihai N <lnicola@stevens.edu>
2023-03-22 01:33:18 +0000

    Unclutter translation AST by removing unnecessary new variables

--- src/effects/translation.ml
+++ src/effects/translation.ml
@@ -73,52 +73,104 @@ let tr (e: term) (a: term) (acfg: term) =
     | Rec (fopt, px, def, l) ->
       let fc = fresh_var "cfg" in
       ret (Rec (fopt, (fc, ""), Rec (None, px, tr_ def (Var (fc, "")), l), "")) acfg
-    | App (e1, e2, l) -> 
-      let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
-      let e2x, acfg2x = mk_fresh_var "x", mk_fresh_var "cfg" in
-      PatMat (tr_ e1 acfg, [
-          mk_pattern_case 
-            (TupleLst ([e1x; acfg1x], ""))
-            (PatMat (tr_ e2 acfg1x, [
-                 mk_pattern_case 
-                   (TupleLst ([e2x; acfg2x], ""))
-                   (App ((mk_app e1x acfg2x), e2x, l))
-               ], ""))
-        ], "")
+    | App (e1, e2, l) ->
+      let tr_e1 = tr_ e1 acfg in
+      begin match tr_e1 with
+        | TupleLst ([e1'; ecfg1], "") ->
+          let tr_e2 = tr_ e2 ecfg1 in
+          begin match tr_e2 with
+            | TupleLst ([e2'; ecfg2], "") ->
+              App ((mk_app e1' ecfg2), e2', l)
+            | _ ->
+              let e2x, acfg2x = mk_fresh_var "x", mk_fresh_var "cfg" in
+              PatMat (tr_e2, [
+                  mk_pattern_case 
+                    (TupleLst ([e2x; acfg2x], ""))
+                    (App ((mk_app e1' acfg2x), e2x, l))
+                ], "")
+          end
+        | _ ->
+          let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
+          let e2x, acfg2x = mk_fresh_var "x", mk_fresh_var "cfg" in
+          PatMat (tr_e1, [
+              mk_pattern_case 
+                (TupleLst ([e1x; acfg1x], ""))
+                (PatMat (tr_ e2 acfg1x, [
+                     mk_pattern_case 
+                       (TupleLst ([e2x; acfg2x], ""))
+                       (App ((mk_app e1x acfg2x), e2x, l))
+                   ], ""))
+            ], "")
+      end
     | UnOp (uop, e1, l) ->
-      let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
-      PatMat (tr_ e1 acfg, [
-          mk_pattern_case 
-            (TupleLst ([e1x; acfg1x], ""))
-            (ret (UnOp (uop, e1x, l)) acfg1x)
-        ], "")
+      let tr_e1 = tr_ e1 acfg in
+      begin match tr_e1 with 
+        | TupleLst ([e1'; ecfg1], "") ->
+          ret (UnOp (uop, e1', l)) ecfg1
+        | _ ->
+          let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
+          PatMat (tr_e1, [
+              mk_pattern_case 
+                (TupleLst ([e1x; acfg1x], ""))
+                (ret (UnOp (uop, e1x, l)) acfg1x)
+            ], "")
+      end
     | BinOp (bop, e1, e2, l) -> 
-      let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
-      let e2x, acfg2x = mk_fresh_var "x", mk_fresh_var "cfg" in
-      PatMat (tr_ e1 acfg, [
-          mk_pattern_case 
-            (TupleLst ([e1x; acfg1x], ""))
-            (PatMat (tr_ e2 acfg1x, [
-                 mk_pattern_case 
-                   (TupleLst ([e2x; acfg2x], ""))
-                   (ret (BinOp (bop, e1x, e2x, l)) acfg2x)
-               ], ""))
-        ], "")
+      let tr_e1 = tr_ e1 acfg in
+      begin match tr_e1 with
+        | TupleLst ([e1'; ecfg1], "") ->
+          let tr_e2 = tr_ e2 ecfg1 in
+          begin match tr_e2 with
+            | TupleLst ([e2'; ecfg2], "") ->
+              ret (BinOp (bop, e1', e2', l)) ecfg2 
+            | _ ->
+              let e2x, acfg2x = mk_fresh_var "x", mk_fresh_var "cfg" in
+              PatMat (tr_e2, [
+                  mk_pattern_case
+                    (TupleLst ([e2x; acfg2x], ""))
+                    (ret (BinOp (bop, e1', e2x, l)) acfg2x)
+                ], "")
+          end
+        | _ ->              
+          let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
+          let e2x, acfg2x = mk_fresh_var "x", mk_fresh_var "cfg" in
+          PatMat (tr_e1, [
+              mk_pattern_case 
+                (TupleLst ([e1x; acfg1x], ""))
+                (PatMat (tr_ e2 acfg1x, [
+                     mk_pattern_case 
+                       (TupleLst ([e2x; acfg2x], ""))
+                       (ret (BinOp (bop, e1x, e2x, l)) acfg2x)
+                   ], ""))
+            ], "")
+      end
     | Ite (b, et, ef, l, asst) -> 
-      let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
-      PatMat (tr_ b acfg, [
-          mk_pattern_case 
-            (TupleLst ([e1x; acfg1x], ""))
-            (Ite (e1x, tr_ et acfg1x, tr_ ef acfg1x, l, asst))
-        ], "")
+      let tr_b = tr_ b acfg in
+      begin match tr_b with
+        | TupleLst ([b'; ecfg1], "") ->
+          Ite (b', tr_ et ecfg1, tr_ ef ecfg1, l, asst)
+        | _ ->
+          let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
+          PatMat (tr_b, [
+              mk_pattern_case 
+                (TupleLst ([e1x; acfg1x], ""))
+                (Ite (e1x, tr_ et acfg1x, tr_ ef acfg1x, l, asst))
+            ], "")
+      end
     | Event (e1, l) -> (print_endline "\nEvent"; print_exp stdout e); 
       ret (Const (UnitLit, "")) (mk_app (mk_app ev_ e1) acfg)
     | PatMat (e1, patlist, l) ->
-      let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
-      PatMat (tr_ e1 acfg, [
-          mk_pattern_case 
-            (TupleLst ([e1x; acfg1x], ""))
-            (PatMat (e1x, (List.map (fun (Case (p, e2)) -> Case (p, tr_ e2 acfg1x)) patlist), l)) 
-        ], "")
+      let tr_e1 = tr_ e1 acfg in
+      begin match tr_e1 with
+        | TupleLst ([e1'; ecfg1], "") ->
+          PatMat (e1', (List.map (fun (Case (p, e2)) -> Case (p, tr_ e2 ecfg1)) patlist), l)
+        | _ ->          
+          let e1x, acfg1x = mk_fresh_var "x", mk_fresh_var "cfg" in
+          PatMat (tr_e1, [
+              mk_pattern_case 
+                (TupleLst ([e1x; acfg1x], ""))
+                (PatMat (e1x, (List.map (fun (Case (p, e2)) -> Case (p, tr_ e2 acfg1x)) patlist), l)) 
+            ], "")
+      end
   in
   mk_app (mk_lambda ev_ (tr_ e acfg)) a 
