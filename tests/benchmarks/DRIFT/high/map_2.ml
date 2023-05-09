e303ce7a9f198f866b0eec37ae42491d5fde614b
culechetoo <chaitanya16027@iiitd.ac.in>
2023-05-09 18:59:53 +0000

    .

--- tests/benchmarks/DRIFT/high/map_2.ml
+++ tests/benchmarks/DRIFT/high/map_2.ml
@@ -1,11 +1,11 @@
 
 
-let rec map1 (f1: int -> int) x1 =
+let rec map1 (f1) x1 =
   if x1 <= 0
   then x1
   else x1 + (f1 (map1 f1 (x1 - 1)))
 
-let rec map2 (f2: int -> int) x2 =
+let rec map2 (f2) x2 =
   if x2 <= 0
   then x2
   else x2 + (f2 (map2 f2 (x2 - 1)))
@@ -14,7 +14,7 @@ let id ix = ix
 
 let succ sx = sx + 1
 
-let main (n(*-:{v:Int | true}*)) =
+let main (n: int) =
 	if n > 0 then
 		assert(map1 id n >= n)
 	else
