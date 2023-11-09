857ea602b57bbcae80f425230136c289f05e9a6f
Eric Koskinen <eric.koskinen@stevens.edu>
2023-11-08 20:14:41 -0500

    more benchmarks into xml

--- tests/effects/ho-shrink.ml
+++ tests/effects/ho-shrink.ml
@@ -9,11 +9,3 @@ let rec shrink t f d =
 let main (gl_t:int) (gl_d:int) =   
     ev (t/d);
     shrink t (fun x -> t) d
-
-(* Dependent *)
-{ 
-	QSet   = [0]; 
-	delta  = fun evx (q, acc) -> (q, (acc+evx));
-    assertFinal = fun (q, acc) -> (acc = t/d);
- 	IniCfg = (0, 0) 
-}
