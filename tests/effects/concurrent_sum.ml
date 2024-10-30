dc66870ede6c5a1a4e70c6836035f6f4f87f2450
culechetoo <chaitanyaagarwal291@gmail.com>
2024-10-30 16:21:51 -0400

    concurrent_sum works

--- tests/effects/concurrent_sum.ml
+++ tests/effects/concurrent_sum.ml
@@ -1,15 +1,26 @@
 
-let choose () = 
-  if nondet then
-    ev 1; 0
-  else if nondet then
-    ev 2; 0
-  else 
-    ev 3; 0
-m
-let rec sum n f =
-  if n = 0 then 0
-  else (f ()) + (sum (n-1) f)
+(* let choose x = 
+  if nondet then begin
+    ev 1; 
+    0
+  end else if nondet then begin
+    ev 2; 
+    0
+  end else begin
+    ev 3;
+    0
+  end *)
 
-let main (n: int(*-:{v:Int | v > 0}*) = 
-  sum n choose
+let rec sum n =
+  if n = 0 then 0
+  else begin
+    if nondet then
+      ev 1
+    else if nondet then
+      ev 2
+    else
+      ev 3;
+    sum (n-1) end
+
+let main (n:int(*-:{v:Int | v>0}*)) = 
+  sum n
