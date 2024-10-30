b7e1a32abc6aeaba3bb821d0d2b184440ae060fe
culechetoo <chaitanyaagarwal291@gmail.com>
2024-10-30 17:32:44 -0400

    Slightly more complex version of nondet_max

--- tests/effects/nondet_max.ml
+++ tests/effects/nondet_max.ml
@@ -2,18 +2,18 @@
   Each number is chosen between 0 and m non-deterministically.
   The property file checks if every chosen number is ≤ m. *)
 
-let rec make_list n m =
+let rec make_list n f =
   if n = 0 then 0
   else begin
-    get_random_int m;
-    make_list (n-1) m
+    ev (f ());
+    make_list (n-1) f
   end
 
-let rec get_random_int n1 =
-  if n1 = 0 then 0
-  else
-    if nondet then get_random_int (n1-1)
-    else begin ev n1; n1 end
-
 let main (n:int(*-:{v:Int | v>0}*)) (m:int(*-:{v:Int | v>0}*)) = 
-  make_list n m
+  let rec get_random_int n1 n2 =
+    if n1 = 0 then 0
+    else
+      if nondet then get_random_int (n1-1) n2
+      else n1
+  in
+  make_list n (get_random_int m)
