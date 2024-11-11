a7c8df33104b3487c0237d709eefa1f0b255cc95
culechetoo <chaitanyaagarwal291@gmail.com>
2024-11-11 18:43:10 -0500

    New benchmark

---
+++ tests/effects/order-irrel-nondet.ml
@@ -0,0 +1,16 @@
+(* 
+there are two particular events (let us treat them as integers
+ c and -c for some c) such that at most one of them is permitted 
+ to occur during any execution
+*)
+let rec order d c b = 
+  if (d > 0) then begin
+     (* begin if ( d mod 2 = 0 ) then ev c else ev(-c) end; *)
+     ev (if b = 1 then c else (-c)); 
+     order (d - 2) c b
+  end else 0
+
+let main (dd:int(*-:{v:Int | true}*)) (cc:int(*-:{v:Int | v > 0}*)) = 
+  let b = if nondet then 1 else 0 in
+  order dd cc b
+
