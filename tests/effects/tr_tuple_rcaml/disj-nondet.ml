ad7e308de79a625d010787b50e40f01e3598f057
Mihai Nicola <lnicola@stevens.edu>
2024-11-12 23:29:38 -0500

    Add RCaml annotations for new benchmarks

---
+++ tests/effects/tr_tuple_rcaml/disj-nondet.ml
@@ -0,0 +1,87 @@
+(*
+Tuple Encoding of Product Program.
+
+Source Program: 
+
+let rec f x b pos neg = 
+  if b = 0 then 
+    ev pos
+  else
+    ev neg;
+  if (x <= 0) then 0 else f (x-1) b pos neg
+
+
+let main (v:int(*-:{v:Int | true}*)) (p:int(*-:{v:Int | true}*)) (n:int(*-:{v:Int | true}*)) =
+  if p > 0 && n < 0 then
+    let bb = if nondet then 1 else 0 in
+    f v bb p n
+  else 0 
+  
+
+
+Property: 
+
+(* Disjunctive *)
+
+QSet   = [0;1;2;3]; 
+
+delta  = fun evx (q, acc) -> 
+       	     if ((q = 0) || (q = 1)) && (evx > 0) then (1,acc)
+	     else if (q = 1) && (evx < 0) then (3, acc)
+       	     else if ((q = 0) || (q = 2)) && (evx < 0) then (2,acc)
+	     else if (q = 2) && (evx > 0) then (3, acc)
+	     else (3, acc);
+
+IniCfg = (0, 0);
+
+(* if event is pos, x must  be even ; *)
+assert = fun (q, acc) -> (q < 3);
+
+
+
+*)
+
+let ev_step0 evx cfg0 =
+  (match cfg0 with 
+   (q,acc) -> if (((q = 0) || (q = 1)) && (evx > 0)) then (1,acc)
+              else if ((q = 1) && (evx < 0)) then (3,acc)
+                   else if (((q = 0) || (q = 2)) && (evx < 0)) then (2,acc)
+                        else if ((q = 2) && (evx > 0)) then (3,acc)
+                             else (3,acc))
+
+
+let ev_step_asst0 cfg1 =
+  (match cfg1 with 
+   (q,acc) -> assert (q < 3))
+
+external nondet_bool : unit -> bool = "unknown"
+
+let rec f x cfg2 =
+  ((fun b cfg3 ->
+      ((fun pos cfg4 ->
+          ((fun neg cfg5 ->
+              (match if (b = 0) then ((fun cfg7 ->
+                                         ((ev_step_asst0 cfg7) ; ((),cfg7))) ((ev_step0 pos) cfg5))
+                     else ((fun cfg6 ->
+                              ((ev_step_asst0 cfg6) ; ((),cfg6))) ((ev_step0 neg) cfg5)) with 
+               (x0,cfg8) -> (match if (x <= 0) then (0,cfg8)
+                                   else
+                                     (match (match (match ((f (x - 1)) cfg8) with 
+                                                    (x2,cfg10) -> ((x2 b) cfg10)) with  (x3,cfg11) -> ((x3 pos) cfg11)) with 
+                                             (x4,cfg12) -> ((x4 neg) cfg12)) with 
+                                     (x1,cfg9) -> ((x0 ; x1),cfg9)))),cfg4)),cfg3)),cfg2) 
+
+
+let main (v:int(*-:{cur_v:Int | true = true}*)) (p:int(*-:{cur_v:Int | true = true}*)) (n:int(*-:{cur_v:Int | true = true}*)) =
+  if
+    ((p > 0) && (n < 0))
+    then
+    let bb0 = if (nondet_bool ()) then (1,(0,0))
+              else (0,(0,0)) in 
+    (match bb0 with 
+     (bb,cfg13) -> (match (match (match ((f v) cfg13) with 
+                                  (x5,cfg14) -> ((x5 bb) cfg14)) with  (x6,cfg15) -> ((x6 p) cfg15)) with 
+                           (x7,cfg16) -> ((x7 n) cfg16))) else (0,(0,0))
+
+
+[@@@assert "typeof(main) <: int -> int -> int -> int * (int * int)"]
---
+++ tests/effects/tr_tuple_rcaml/order-irrel-nondet.ml
@@ -0,0 +1,81 @@
+external nondet_bool : unit -> bool = "unknown"external nondet_bool : unit -> bool = "unknown"(*
+Tuple Encoding of Product Program.
+
+Source Program: 
+
+(* 
+there are two particular events (let us treat them as integers
+ c and -c for some c) such that at most one of them is permitted 
+ to occur during any execution
+*)
+let rec order d c b = 
+  if (d > 0) then begin
+     (* begin if ( d mod 2 = 0 ) then ev c else ev(-c) end; *)
+     ev (if b = 1 then c else (-c)); 
+     order (d - 2) c b
+  end else 0
+
+let main (dd:int(*-:{v:Int | true}*)) (cc:int(*-:{v:Int | v > 0}*)) = 
+  let bb = if nondet then 1 else 0 in
+  order dd cc bb
+
+
+
+Property: 
+
+(* there are two particular events (let us treat them as integers
+ c and -c for some c) such that at most one of them is permitted 
+ to occur during any execution *)
+
+QSet   = [0;1]; 
+
+delta  = fun evx (q, acc) -> 
+    if q = 1 then (1, acc)
+    else if acc = 0 then (q, evx) 
+    else if acc = evx then (q, acc)
+    else (1,acc);
+
+IniCfg = (0, 0);
+
+assertFinal = fun (q, acc) -> q = 0;
+
+
+*)
+
+let ev_step0 evx cfg0 =
+  (match cfg0 with 
+   (q,acc) -> if (q = 1) then (1,acc)
+              else if (acc = 0) then (q,evx)
+                   else if (acc = evx) then (q,acc)
+                        else (1,acc))
+
+
+let asst_final0 cfg1 =
+  (match cfg1 with 
+   (q,acc) -> assert (q = 0))
+
+external nondet_bool : unit -> bool = "unknown"
+
+let rec order d cfg2 =
+  ((fun c cfg3 ->
+      ((fun b cfg4 ->
+          if
+            (d > 0)
+            then
+            (match (match if (b = 1) then (c,cfg4)
+                          else ((- c),cfg4) with 
+                    (x0,cfg5) -> ((),((ev_step0 x0) cfg5))) with 
+                   (x1,cfg6) -> (match (match (match ((order (d - 2)) cfg6) with 
+                                               (x3,cfg8) -> ((x3 c) cfg8)) with  (x4,cfg9) -> ((x4 b) cfg9)) with 
+                                        (x2,cfg7) -> ((x1 ; x2),cfg7)))
+                     else (0,cfg4)),cfg3)),cfg2) 
+
+
+let main (dd:int(*-:{cur_v:Int | true = true}*)) (cc:int(*-:{cur_v:Int | cur_v = 0}*)) =
+  (match let bb0 = if (nondet_bool ()) then (1,(0,0))
+                   else (0,(0,0)) in 
+         (match bb0 with 
+          (bb,cfg10) -> (match (match ((order dd) cfg10) with 
+                                (x5,cfg11) -> ((x5 cc) cfg11)) with  (x6,cfg12) -> ((x6 bb) cfg12))) with  (e0,acfg0) -> ((asst_final0 acfg0) ; e0))
+
+[@@@assert "typeof(main) <: int -> int -> int"]
