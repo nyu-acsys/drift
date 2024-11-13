1e1e8da7963a9b64ba551b055180404f36395fb5
Mihai Nicola <lnicola@stevens.edu>
2024-11-13 00:19:06 -0500

    Tuple translations for newly added benchmarks

---
+++ tests/effects/tr_tuple/concurrent_sum.ml
@@ -0,0 +1,77 @@
+(*
+Tuple Encoding of Product Program.
+
+Source Program: 
+
+
+let choose x =
+  if nondet then begin
+    ev 1;
+    0
+  end else if nondet then begin
+    ev 2;
+    0
+  end else begin
+    ev 3;
+    0
+  end
+
+let rec sum n =
+  if n = 0 then 0
+  else begin
+    (* if nondet then *)
+    (*   ev 1 *)
+    (* else if nondet then *)
+    (*   ev 2 *)
+    (* else *)
+    (*   ev 3; *)
+    choose ();
+    sum (n-1) end
+
+let main (n:int(*-:{v:Int | v>0}*)) = 
+  sum n
+
+
+Property: 
+
+QSet   = [0;1]; 
+
+delta  = fun evx (q, acc) -> 
+  if   (q = 0 && evx <= 3) then (0, evx + acc)
+  else (1, acc);
+
+IniCfg = (0, 0);
+
+assertFinal = fun (q, acc) -> q = 0 && acc <= 3*prefn;
+
+
+*)
+
+let main (prefn:int(*-:{cur_v:Int | cur_v = 0}*)) =
+
+let ev_step0 evx cfg0 =
+  (match cfg0 with 
+   (q,acc) -> if ((q = 0) && (evx <= 3)) then (0,(evx + acc))
+              else (1,acc))
+in
+
+let asst_final0 cfg1 =
+  (match cfg1 with 
+   (q,acc) -> assert ((q = 0) && (acc <= (3 * prefn))))
+in
+
+let choose x cfg2 =
+  if (nondet_op) then ((() ; 0),((ev_step0 1) cfg2))
+  else if (nondet_op) then ((() ; 0),((ev_step0 2) cfg2))
+       else ((() ; 0),((ev_step0 3) cfg2))
+in
+
+let rec sum n cfg3 =
+  if (n = 0) then (0,cfg3)
+  else (match ((choose ()) cfg3) with 
+        (x0,cfg4) -> (match ((sum (n - 1)) cfg4) with 
+                      (x1,cfg5) -> ((x0 ; x1),cfg5))) 
+in
+
+  (match ((sum prefn) (0,0)) with 
+   (e0,acfg0) -> ((asst_final0 acfg0) ; e0))
---
+++ tests/effects/tr_tuple/nums_evens.ml
@@ -0,0 +1,74 @@
+(*
+Tuple Encoding of Product Program.
+
+Source Program: 
+
+(* A program emits an event every alternate iteration of f.
+  It makes sure that the overall number of effects is smaller
+  than half of m. *)
+
+let main (m:int(*-:{v:Int | v>0}*)) =
+  let flip x =
+    if x = 1 then 0
+    else 1
+  in
+  let rec f m1 flag =
+    if m1 = 0 then ()
+    else
+      (if flag=1 then begin ev 1; f (m1-1) end
+      else f (m1-1))
+      (flip flag)
+  in
+  f m 0
+
+
+Property: 
+
+QSet   = [0;1;1]; 
+
+delta  = fun evx (q, acc) -> 
+  if   (q = 0 && evx=1) then (0, acc+1)
+  else (1, acc);
+
+IniCfg = (0, 0);
+
+assert = fun (q, acc) -> (q = 0 && (acc+acc <= prefm));
+
+*)
+
+let main (prefm:int(*-:{cur_v:Int | cur_v = 0}*)) =
+
+let ev_step0 evx cfg0 =
+  (match cfg0 with 
+   (q,acc) -> if ((q = 0) && (evx = 1)) then (0,(acc + 1))
+              else (1,acc))
+in
+
+let ev_step_asst0 cfg1 =
+  (match cfg1 with 
+   (q,acc) -> assert ((q = 0) && ((acc + acc) <= prefm))) 
+in
+
+
+  let flip0 = ((fun x cfg12 ->
+                  if (x = 1) then (0,cfg12)
+                  else (1,cfg12)),(0,0)) in 
+  (match flip0 with 
+   (flip,cfg2) -> let f0 = ((let rec f m1 cfg5 =
+                               ((fun flag cfg6 ->
+                                   if (m1 = 0) then ((),cfg6)
+                                   else
+                                     (match if
+                                              (flag = 1)
+                                              then
+                                              (match ((fun cfg7 ->
+                                                         ((ev_step_asst0 cfg7) ; ((),cfg7))) ((ev_step0 1) cfg6)) with 
+                                               (x1,cfg8) -> (match ((f (m1 - 1)) cfg8) with 
+                                                             (x2,cfg9) -> ((x1 ; x2),cfg9)))
+                                              else ((f (m1 - 1)) cfg6) with 
+                                            (x3,cfg10) -> (match ((flip flag) cfg10) with 
+                                                           (x4,cfg11) -> ((x3 x4) cfg11)))),cfg5)
+                                     in f),cfg2) in 
+                               (match f0 with 
+                                (f,cfg3) -> (match ((f prefm) cfg3) with 
+                                             (x0,cfg4) -> ((x0 0) cfg4))))
