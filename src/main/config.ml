70285e23f00906db4b5abb5d91aa7028f60f09a0
culechetoo <chaitanyaagarwal291@gmail.com>
2025-06-24 18:31:33 -0400

    config option changes

--- src/main/config.ml
+++ src/main/config.ml
@@ -31,35 +31,35 @@ let prop_file = ref ""
 let convert_to_cps = ref false
 let convert_to_tuple = ref false
 
-let usage = "usage: " ^ Sys.argv.(0) ^ " [-file <file name>] [-domain <domain name>] [-thold <true/false>] [-delay-wid <num>] [-nar <true/false>] [-prop <file name>] [-ev-trans <true/false>] [-cps-convert <true/false>] [-out <num>] [-debug] [-bt]"
+let usage = "usage: " ^ Sys.argv.(0) ^ " [-file <file name>] [-domain <domain name>] [-thold <true/false>] [-prop <file name>] [-trace-len <num>] [-if-part <true/false>] [-io-effects <true/false>] [-ev-trans <true/false>] [-cps-convert <true/false>] [-tuple-convert <true/false>] [-out <num:[0,2]>] [-debug] [-bt]"
 
 let cmd_options_spec =
   [("-file", Arg.String (fun s -> parse_file := true; file := s), ": Input file specification");
   ("-domain", Arg.String (fun s -> domain := s), ": Abstract domain specification (Oct, Polka_st, Polka_ls, PolkaGrid)");
    ("-thold", Arg.String (fun s -> if s = "true" then use_threshold:=true else use_threshold:=false), ": Use threshold widening");
-   ("-delay-wid", Arg.Int (fun s -> delay_wid := s), ": Set number of delay widening steps (depricated)");
-   ("-nar", Arg.String (fun s -> if s = "true" then narrow:=true else narrow:=false), ": Use narrowing procedure (depricated)");
-   ("-out", Arg.Int (fun s -> out_put_level := s), 
-    ": Output result level\n
-      \t 0: Output map after each step\n
-      \t 1: Output map only for the last step\n
-      \t 2: Output the result only");
-   ("-debug", Arg.Set debug, ": Debug mode");
-   ("-profile", Arg.Set profile, ": Profiling output");
-   ("-bt", Arg.Set bt, ": Allow trace back");
-   ("-prop", Arg.String (fun s -> effect_on := true; prop_file := s), ": Automata specification of safety property for effect analysis");
-   ("-ev-trans", Arg.String (fun s -> if s = "true" then ev_trans := true else ev_trans := false), ": Translate Ev expressions"); 
-   ("-io-effects", Arg.String (fun s -> if (s = "true" && !effect_on && (not !ev_trans)) then io_effect_on := true else io_effect_on := false), ": Input-output relations for effects for greater precision"); 
    ("-trace-len", Arg.Int (fun s -> trace_len := s), ": Set maximum allowed trace length. 0 -> not context sensitive");
    ("-if-part", Arg.String (fun s -> if s = "true" then if_part:=true else if_part:=false), ": Partition traces on if tokens");
+   (* ("-delay-wid", Arg.Int (fun s -> delay_wid := s), ": Set number of delay widening steps (depricated)"); *)
+   (* ("-nar", Arg.String (fun s -> if s = "true" then narrow:=true else narrow:=false), ": Use narrowing procedure (depricated)"); *)
+   ("-prop", Arg.String (fun s -> effect_on := true; prop_file := s), ": Automata specification of safety property for effect analysis");
+   ("-io-effects", Arg.String (fun s -> if (s = "true" && !effect_on && (not !ev_trans)) then io_effect_on := true else io_effect_on := false), ": Input-output relations for effects for greater precision"); 
+   ("-ev-trans", Arg.String (fun s -> if s = "true" then ev_trans := true else ev_trans := false), ": Translate Ev expressions"); 
    ("-cps-convert", Arg.String (fun s -> if s = "true" then (convert_to_cps:=true; ev_trans := false) 
                                       else convert_to_cps:=false), ": Convert prog to CPS");
    ("-tuple-convert", Arg.String (fun s -> if s = "true" then (convert_to_tuple:=true; ev_trans := true) 
                                       else convert_to_tuple:=false), ": Convert prog to tuple-encoding of the product program");
+   ("-out", Arg.Int (fun s -> out_put_level := s), 
+    ": Output result level
+      \t 0: Output map after each step
+      \t 1: Output map only for the last step
+      \t 2: Output the result only");
+   ("-debug", Arg.Set debug, ": Debug mode");
+   ("-profile", Arg.Set profile, ": Profiling output");
+   ("-bt", Arg.Set bt, ": Allow trace back");
   ]
 
 (* Parse auxiliary 'command line options' that are set during parsing of the input file *)
-let parse_options options =
+(* let parse_options options =
   (if (!debug) then Printf.printf "Setting options: %s\n" options);
   let options = Sys.argv.(0) :: Str.split_delim (Str.regexp "[ \t\n]+") options |> Array.of_list in
   let current = ref 0 in
@@ -71,7 +71,8 @@ let parse_options options =
       if matched then Str.matched_group 1 full_msg 
       else "invalid option"
     in
-    raise (Invalid_argument msg)
+    raise (Invalid_argument msg) *)
 
 
 let parse = Arg.parse cmd_options_spec (fun x -> raise (Arg.Bad ("Bad argument : " ^ x))) usage;
+if String.equal !domain "PolkaGrid" && !use_threshold then raise (Arg.Bad ("Bad argument : Thresholding with PolkaGrid domain is unsound."));
