f50fd9acc7528ea3ba8b841ae5ff817a1e305946
Mihai Nicola <lnicola@stevens.edu>
2025-07-17 19:30:47 -0400

    comment unused function in bench 'higher-order'

--- tests/effects/higher-order.ml
+++ tests/effects/higher-order.ml
@@ -1,7 +1,7 @@
 let rec apply f x = (ev 1; if x <= 0 then 0 else ((apply f (f x))))
 
 let pred x = x - 1
-let succ x = x + 1
+(* let succ x = x + 1 *)
 
 let main (n:int(*-:{v:Int | true}*)) =
   let _ = apply pred n in
